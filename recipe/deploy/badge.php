e2899a4a98cedf4abcc03a3b3a2ea9eb20a5b863
Jens Descamps <jens@digicreate.be>
2018-08-29 19:29:55 +0200

    Added badge support

---
+++ recipe/deploy/badge.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Deployer;
+
+desc('Add a badge to your repository');
+
+set('latestTag', function() {
+    $latestTag = runLocally('git describe --abbrev=0 --tags');
+
+    return $latestTag;
+});
+
+before('deploy', function() {
+    if(!has('apiKey')) {
+        writeln('<fg=red>No Gitlab `apiKey` set, no badge will be added.</fg=red>');
+    }
+});
+
+after('success', 'git:badge');
+task('git:badge', function () {
+        $hostname = get('hostname');
+        set('anchor', str_replace('.', '', get('latestTag')));
+        if(has($hostname . '_badge_id')) {
+            set('badge_id', get($hostname . '_badge_id'));
+            set('color', has('production_badge_id') ? get($hostname . '_badge_id') : 'brightgreen');
+            runLocally('curl --request PUT -H "PRIVATE-TOKEN: {{apiKey}}" -F "link_url=https://gitlab.com/%{project_path}#version-{{anchor}}" -F "image_url=https://img.shields.io/badge/{{hostname}}-v{{latestTag}}-{{color}}.svg" https://gitlab.com/api/v4/projects/{{project_id}}/badges/{{badge_id}}');
+            writeln('<info>Badge added</info>');
+        } else {
+            writeln('<fg=red>No `{{hostname}}_badge_id` set, could not add badge</fg=red>');
+        }
+});
